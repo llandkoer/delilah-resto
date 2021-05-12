80077f87de877b42b3650c06b1e5d7e194f0f623
Luis Landkoer <llandkoer@gmail.com>
2021-05-12 12:58:40 -0500

    Create auth controllers

---
+++ src/controllers/authController.js
@@ -0,0 +1,17 @@
+const sequelize = require("../connection");
+
+const createUser = (req, res, next) => {
+
+};
+
+const loginUser = (req, res, next) => {
+
+};
+
+const verifyToken = (req, res, next) => {
+
+};
+
+module.exports = createUser;
+module.exports = loginUser;
+module.exports = verifyToken;
