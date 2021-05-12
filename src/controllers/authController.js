1f9bba04fe9952683dba70334ae99ee0f2e6ebde
Luis Landkoer <llandkoer@gmail.com>
2021-05-12 13:34:49 -0500

    Update auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,36 @@
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
+
 const sequelize = require("../connection");
 
-const createUser = (req, res, next) => {
+const encryptPassword = require("../middlewares/encryptPassword");
 
+const createUser = async (req, res, next) => {
+  const { username, full_name, email, phone_number, address, role_id, password } = req.body;
+  const arrayInsertUser = [`${username}`, `${full_name}`, `${email}`, `${phone_number}`, `${address}`, `${role_id}`, `${password}`];
+
+  try {
+    arrayInsertUser[6] = await encryptPassword(password);
+    const result = await sequelize.query(
+      "INSERT INTO users(username, full_name, email, phone_number, address, role_id, password) VALUES( ?, ?, ?, ?, ?, ?, ?)",
+      { replacements: arrayInsertUser, type: sequelize.QueryTypes.INSERT },
+    );
+
+    const token = jwt.sign({}, process.env.JWT_KEY, {
+      expiresIn: 60 * 60 * 24,
+    });
+
+    console.log(result);
+    res.status(201).json({ auth: true, token });
+  } catch (error) {
+    console.log(`Insertion error: ${error}`);
+  }
 };
 
-const loginUser = (req, res, next) => {
+const loginUser = (req, res, next) => {};
 
-};
-
-const verifyToken = (req, res, next) => {
-
-};
+const verifyToken = (req, res, next) => {};
 
 module.exports = createUser;
 module.exports = loginUser;
