912db3582c9b245cd10aa2c22ba4e6d975c604e8
Luis Landkoer <llandkoer@gmail.com>
2021-05-12 12:06:11 -0500

    Update auth routes

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const router = Router();
 
+const authController = require("../controllers/authController");
+
 router.post("/signup");
 
 router.post("/login");
