dde9497dd0a36c5a80abcb317834b2cf4f065950
Luis Landkoer <llandkoer@gmail.com>
2021-05-17 20:52:50 -0500

    Update application

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const app = express();
 
+const authRoute = require("./routes/authRoute");
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use(require("./routes/authRoute"));
+app.use("/api/v1.0/users", authRoute);
 
 module.exports = app;
