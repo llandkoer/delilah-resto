008784570c8517f0a45fdf3092d537c2d7757aca
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-05-20 23:57:35 -0500

    feat get and create products

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 const authRoute = require("./routes/authRoute");
+const productRoute = require("./routes/productRoute")
 
 // Middlewares
 app.use(express.json());
@@ -10,5 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api/v1.0/users", authRoute);
+
+app.use("/api/v1.0/products", productRoute);
 
 module.exports = app;
