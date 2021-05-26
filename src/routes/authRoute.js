7328fd9121e89747901e01ac282ba471b63a1f15
Luis Landkoer <llandkoer@gmail.com>
2021-05-26 18:41:30 -0500

    Update auth routes

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,11 +3,12 @@ const { Router } = require("express");
 const router = Router();
 
 const authController = require("../controllers/authController");
+const authMiddlewares = require("../middlewares/authMiddlewares");
 
-router.post("/signup", authController.createUser);
+router.post("/signup", authMiddlewares.signupValidations(), authController.createUser);
 
-router.post("/login");
+router.post("/login", authMiddlewares.loginValidations(), authController.loginUser);
 
-router.get("/me");
+router.get("/me", authController.verifyToken);
 
 module.exports = router;
