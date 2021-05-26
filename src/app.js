c2b57c7a5babf2e0eebe67d188fa1fe4a475db1c
Luis Landkoer <llandkoer@gmail.com>
2021-05-26 18:41:47 -0500

    Update application

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const morgan = require("morgan");
+
+const helmet = require("helmet");
+
 const app = express();
 
 const authRoute = require("./routes/authRoute");
@@ -7,6 +11,8 @@ const authRoute = require("./routes/authRoute");
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(helmet());
 
 // Routes
 app.use("/api/v1.0/users", authRoute);
