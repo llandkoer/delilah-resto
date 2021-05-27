ee3db7ebd1a96bcc021eb170c951c9379b1557a6
Luis Landkoer <llandkoer@gmail.com>
2021-05-26 19:58:46 -0500

    Update verify token middleware

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,60 +1,30 @@
-const isClient = (req, res, next) => {
-  try {
-    const token = req.headers["x-access-token"];
-    if (!token) {
-      return res.status(401).json({ auth: false, message: "No token provided" });
+const jwt = require("jsonwebtoken");
+
+const { config } = require("../config/config");
+
+const verifyToken = (number, role) => {
+  return (req, res, next) => {
+    try {
+      const token = req.headers["x-access-token"];
+      if (!token) {
+        return res.status(401).json({ message: "No token provided" });
+      }
+
+      jwt.verify(token, config.jwt.secretKey, (error, decoded) => {
+        if (error) {
+          return res.status(401).json({ message: "Token expired" });
+        }
+
+        if (decoded.role !== number) {
+          return res.status(401).json({ message: `User is not ${role}` });
+        }
+        next();
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "There was an error on server" });
     }
+  };
+};
 
-    const decoded = jwt.verify(token, config.jwt.secretKey);
-    if(decoded.role !== 2){
-      return res.status(401).json({ auth: false, message: "User is not client" });
-    }
-
-    let result = await sequelize.query(`SELECT user_id, username, full_name, email, phone_number, address, role_id FROM users WHERE user_id = ?`, {
-      replacements: [`${decoded.id}`],
-      type: sequelize.QueryTypes.SELECT,
-    });
-
-    result = result[0];
-
-    if (!result) {
-      return res.status(404).json({ auth: false, message: "No user found" });
-    }
-    res.status(200).json({ user: result });
-  } catch (error) {
-    console.log(error);
-  }
-  next();
-}
-
-const isAdmin = (req, res, next) => {
-  try {
-    const token = req.headers["x-access-token"];
-    if (!token) {
-      return res.status(401).json({ auth: false, message: "No token provided" });
-    }
-
-    const decoded = jwt.verify(token, config.jwt.secretKey);
-    if(decoded.role !== 1){
-      return res.status(401).json({ auth: false, message: "User is not admin" });
-    }
-
-    let result = await sequelize.query(`SELECT user_id, username, full_name, email, phone_number, address, role_id FROM users WHERE user_id = ?`, {
-      replacements: [`${decoded.id}`],
-      type: sequelize.QueryTypes.SELECT,
-    });
-
-    result = result[0];
-
-    if (!result) {
-      return res.status(404).json({ auth: false, message: "No user found" });
-    }
-    res.status(200).json({ user: result });
-  } catch (error) {
-    console.log(error);
-  }
-  next();
-}
-
-exports.isClient = isClient;
-exports.isAdmin = isAdmin;
+exports.verifyToken = verifyToken;
