0378fcd4f5a489865fd687f4246a9cd1407e4038
Luis Landkoer <llandkoer@gmail.com>
2021-05-26 22:39:36 -0500

    Update verify token middleware

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -2,28 +2,26 @@ const jwt = require("jsonwebtoken");
 
 const { config } = require("../config/config");
 
-const verifyToken = (number, role) => {
-  return (req, res, next) => {
-    try {
-      const token = req.headers["x-access-token"];
-      if (!token) {
-        return res.status(401).json({ message: "No token provided" });
+const verifyToken = (roleId, role) => (req, res, next) => {
+  try {
+    const token = req.headers["x-access-token"];
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    jwt.verify(token, config.jwt.secretKey, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ message: "Token expired" });
       }
 
-      jwt.verify(token, config.jwt.secretKey, (error, decoded) => {
-        if (error) {
-          return res.status(401).json({ message: "Token expired" });
-        }
-
-        if (decoded.role !== number) {
-          return res.status(401).json({ message: `User is not ${role}` });
-        }
-        next();
-      });
-    } catch (error) {
-      res.status(500).json({ error, message: "There was an error on server" });
-    }
-  };
+      if (decoded.role !== roleId) {
+        return res.status(401).json({ message: `User is not ${role}` });
+      }
+      next();
+    });
+  } catch (error) {
+    res.status(500).json({ error, message: "There was an error on server" });
+  }
 };
 
 exports.verifyToken = verifyToken;
