c10dd22feef87091ce0d8cc9b6ed573d56a84c59
Luis Landkoer <llandkoer@gmail.com>
2021-05-31 15:54:36 -0500

    Create route: get all orders

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -4,12 +4,15 @@ const { config } = require("../config/config");
 
 const sequelize = require("../connection");
 
-const foo = async (req, res) => {
+const getAllOrders = async (req, res) => {
   try {
-
+    let orders = await sequelize.query(`SELECT * FROM orders`, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+    res.status(200).json({ orders });
   } catch (error) {
-
+    res.status(500).json({ error, message: "There is a mistake on server" });
   }
 }
 
-exports.foo = foo;
+exports.getAllOrders = getAllOrders;
--- src/routes/ordersRoute.js
+++ src/routes/ordersRoute.js
@@ -3,10 +3,18 @@ const { Router } = require("express");
 const router = Router();
 
 const ordersController = require("../controllers/ordersController");
-const ordersMiddlewares = require("../middlewares/ordersMiddlewares");
+// const ordersMiddlewares = require("../middlewares/ordersMiddlewares");
+const verifyToken = require("../middlewares/verifyToken");
 
-router.post("/", ordersMiddlewares.(), ordersController.);
+const { config } = require("../config/config");
 
-router.post("/", ordersMiddlewares.(), ordersController.);
+const admin = {
+  id:config.jwt.admin,
+  label: "admin",
+};
+
+router.get("/all-orders", verifyToken.verifyToken(admin.id, admin.label), ordersController.getAllOrders);
+
+// router.post("/", ordersMiddlewares...(), ordersController....);
 
 module.exports = router;
