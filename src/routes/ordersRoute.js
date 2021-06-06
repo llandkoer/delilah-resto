d8f3dd182b904b90adcb98a694b36262204fe881
Luis Landkoer <llandkoer@gmail.com>
2021-06-06 00:30:01 -0500

    Update orders routes

--- src/routes/ordersRoute.js
+++ src/routes/ordersRoute.js
@@ -13,12 +13,19 @@ const admin = {
   label: "admin",
 };
 
-// const user = {
-//   id: config.jwt.user,
-//   label: "user"
-// }
+const user = {
+  id: config.jwt.user,
+  label: "client"
+}
 
 router.get("/all-orders", verifyToken.verifyToken(admin.id, admin.label), ordersController.getAllOrders);
+router.get("/order-states", verifyToken.verifyToken(admin.id, admin.label), ordersController.getOrderStates);
+router.get("/payment-methods", verifyToken.verifyToken(admin.id, admin.label), ordersController.getPaymentMethods);
 router.put("/update-state/:id", verifyToken.verifyToken(admin.id, admin.label), ordersMiddlewares.updateOrderStateValidations(), ordersController.updateOrderState);
+
+router.post("/create-order", verifyToken.verifyToken(user.id, user.label), ordersMiddlewares.createOrderValidations(),ordersController.createOrder);
+router.get("/all-favorites", verifyToken.verifyToken(user.id, user.label), ordersController.getFavorites);
+router.post("/set-favorite", verifyToken.verifyToken(user.id, user.label), ordersMiddlewares.setFavoriteValidations(),ordersController.setFavorite);
+
 
 module.exports = router;
