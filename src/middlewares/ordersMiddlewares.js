b99bc497fc0353f26e080489b66a2accba991e9c
Luis Landkoer <llandkoer@gmail.com>
2021-06-06 00:29:50 -0500

    Update orders middlewares

--- src/middlewares/ordersMiddlewares.js
+++ src/middlewares/ordersMiddlewares.js
@@ -9,3 +9,22 @@ exports.updateOrderStateValidations = () => [
     .isLength({ max: 1 })
     .withMessage("State ID is not valid"),
 ];
+
+exports.createOrderValidations = () => [
+  check("payment_method_id")
+    .notEmpty()
+    .withMessage("Payment method ID is required")
+    .isInt({ min: 1, max: 2 })
+    .withMessage("Payment method ID is not valid"),
+  check("products")
+    .notEmpty()
+    .withMessage("Products ID array is required"),
+];
+
+exports.setFavoriteValidations = () => [
+  check("product_id")
+    .notEmpty()
+    .withMessage("Product ID is required")
+    .isInt()
+    .withMessage("Product ID is not valid")
+]
