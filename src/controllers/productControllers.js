0c2b6767df7a709611b3b8935b64c45ba0bcb9ad
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-06-07 12:05:47 -0500

    update products

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -10,22 +10,28 @@ const createProduct = async (req, res) => {
   const arrayInsertProduct = [`${name}`, `${price}`, `${photo}`]
 
   try {
-    const send = await sequelize.query(
-      "INSERT INTO products(name, price, photo) VALUES( ?, ?, ?)", {
-        replacements: arrayInsertProduct,
-        type: sequelize.QueryTypes.INSERT
+
+    if (name == undefined || price == undefined || photo == undefined) {
+      res.status(400).json({
+        message: "enter all data (name, price and photo(url))"
       })
-    res.status(200).json({
-      messange: "Producto creado exitosamente"
-    })
-    console.log(send)
+    } else {
+      const send = await sequelize.query(
+        "INSERT INTO products(name, price, photo) VALUES( ?, ?, ?)", {
+          replacements: arrayInsertProduct,
+          type: sequelize.QueryTypes.INSERT
+        })
+      res.status(200).json({
+        messange: "Producto creado exitosamente"
+      })
+    }
   } catch (error) {
     console.error(error)
   }
 
 }
 
-const getAllProducts = async (req,res) => {
+const getAllProducts = async (req, res) => {
   try {
     const response = await sequelize.query('SELECT * FROM products ORDER BY price ASC')
     res.status(200).send(response[0])
@@ -37,7 +43,72 @@ const getAllProducts = async (req,res) => {
   }
 }
 
+const deleteProduct = async (req, res) => {
+
+  try {
+    const {
+      id
+    } = req.query;
+    console.log(id)
+    //haga un select guarda en una variable el select si exite 
+    let confirm = await sequelize.query(`SELECT * FROM products WHERE product_id = ${id}`)
+    console.log(confirm[0].length)
+    
+    if (confirm[0].length > 0) {
+      let resp = await sequelize.query(`DELETE FROM products WHERE product_id = ${id}`, {
+        type: sequelize.QueryTypes.DELETE,
+      })
+      console.log(resp)
+      res.status(200).json({
+        messange: "Product successfully deleted"
+      })
+    } 
+    else {
+      res.status(400).json({
+        message: "id not existing"
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      error,
+      message: "There is a mistake on server"
+    })
+  }
+}
+
+const putProduct = async (req, res) => {
+
+  try {
+
+    const {
+      id
+    } = req.params
+    let {
+      name,
+      price,
+      photo
+    } = req.body
+
+    resp = await sequelize.query(`UPDATE products SET name = '${name}', price = ${price}, photo = '${photo}' WHERE product_id = ${id}`, {
+      type: sequelize.QueryTypes.UPDATE,
+    })
+
+    res.status(200).json({
+      message: "Product successfully updated"
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+
+
+}
+
+exports.deleteProduct = deleteProduct;
 
 exports.createProduct = createProduct;
 
 exports.getAllProducts = getAllProducts
+
+exports.putProduct = putProduct
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,11 +1,30 @@
-const { Router } = require("express");
+const {
+  Router
+} = require("express");
 
 const router = Router();
 
+const verifyToken = require("../middlewares/verifyToken");
+
+const {
+  config
+} = require("../config/config");
+
+const admin = {
+  id: config.jwt.admin,
+  label: "admin",
+};
+
+
 const productControllers = require("../controllers/productControllers");
 
-router.post("/create", productControllers.createProduct);
+
+router.post("/create", verifyToken.verifyToken(admin.id, admin.label), productControllers.createProduct);
 
 router.get("/allproducts", productControllers.getAllProducts);
+
+router.delete("/delete", productControllers.deleteProduct);
+
+router.put('/update/:id', productControllers.putProduct)
 
 module.exports = router;
